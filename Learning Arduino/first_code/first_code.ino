f9f4d77ff89d999755b1332ae5916e5f17452d77
Arpan Mukherjee <arpanmukherjee965@gmail.com>
2024-05-02 23:25:14 +0530

    .

--- Learning Arduino/first_code/first_code.ino
+++ Learning Arduino/first_code/first_code.ino
@@ -1,9 +1,11 @@
-int mydelay = 10000;
-
 void setup(){
-    Serial.begin(9600);
+    pinMode(LED_BUILTIN,OUTPUT);
+
 }
 
 void loop(){
-    Serial.println("Hello");
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN,LOW);
+    delay(1000);
 }
